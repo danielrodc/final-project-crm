eef791632608c88c1bd87571856ac6d045ad25b2
Jose Zangarini <jzangarinim@gmail.com>
2023-07-03 16:08:26 -0400

    modified color scheme/design

--- src/front/js/component/navbar.js
+++ src/front/js/component/navbar.js
@@ -3,7 +3,7 @@ import { Link } from "react-router-dom";
 
 export const Navbar = () => {
   return (
-    <nav className="navbar navbar-expand-lg bg-light">
+    <nav className="navbar navbar-expand-lg">
       <div className="container-fluid">
         <a className="navbar-brand text-success" href="#">
           Success CRM
--- src/front/js/pages/home.js
+++ src/front/js/pages/home.js
@@ -1,17 +1,13 @@
-import React, { useContext } from "react";
+import React, { useState, useContext } from "react";
 import { Context } from "../store/appContext";
 import "../../styles/home.css";
-import mySvg from "../../img/layered-waves-haikei.svg";
 
 export const Home = () => {
   const { store, actions } = useContext(Context);
   return (
     <>
-      <div
-        className="container-fluid d-flex justify-content-center"
-        style={{ backgroundImage: `url(${mySvg})` }}
-      >
-        <div className="col-10 card mb-3 mt-3">
+      <div className="container-fluid d-flex justify-content-center">
+        <div className="col-12 card mb-3 mt-3 border border-success">
           <div className="row g-0">
             <div className="col-md-8 d-flex align-items-center">
               <img
@@ -20,7 +16,7 @@ export const Home = () => {
                 alt="..."
               />
             </div>
-            <div className="col-md-4">
+            <div className="col-md-4 test">
               <div className="card-body d-flex flex-column align-items-center justify-content-center h-100">
                 <button type="button" className="btn btn-success mb-3">
                   Log in
