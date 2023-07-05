6df08e35d01ce22ce7dada5005439fabe827b830
Jose Zangarini <jzangarinim@gmail.com>
2023-07-05 16:11:01 -0400

    added function to send email to employee on click

--- src/front/js/pages/Employees.jsx
+++ src/front/js/pages/Employees.jsx
@@ -104,12 +104,18 @@ export const Employees = (props) => {
                     </div>
                     <div className="col-7 w-auto">
                       <div className="card-body">
-                        <h5 className="card-title">{`${employee?.name} ${employee.last_name}`}</h5>
+                        <h5 className="card-title">{`${employee?.name} ${employee?.last_name}`}</h5>
                         <h6 className="card-subtitle mb-2 text-body-secondary">
                           {`${employee?.department} - ${employee?.role}`}
                         </h6>
                         <p className="card-text">{`email: ${employee?.email}`}</p>
-                        <a href="#" className="card-link">
+                        <a
+                          href="#"
+                          className="card-link"
+                          onClick={() =>
+                            (window.location = `mailto:${employee?.email}`)
+                          }
+                        >
                           Schedule a meeting with {employee?.name}
                         </a>
                       </div>
