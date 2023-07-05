73c9ca0d77ef9c708926b487229aa2726af813fa
Jose Zangarini <jzangarinim@gmail.com>
2023-07-05 18:36:03 -0400

    added /customers/id endpoints, created Customer() table, updated Virtualassistant and Customer with __repr__ and serialize() methods

--- src/api/routes.py
+++ src/api/routes.py
@@ -2,11 +2,12 @@
 This module takes care of starting the API Server, Loading the DB and Adding the endpoints
 """
 from flask import Flask, request, jsonify, url_for, Blueprint
-from api.models import db, User
+from api.models import db, User, Customer
 from api.utils import generate_sitemap, APIException
 
 api = Blueprint('api', __name__)
 
+# /users endpoints
 @api.route('/users', methods=['GET'])
 def get_users():
     users = User()
@@ -39,11 +40,22 @@ def get_department(department = None):
     else:
         return jsonify({"message":"bad request"}), 400
 
-@api.route('/hello', methods=['POST', 'GET'])
-def handle_hello():
+# /customers endpoints
+@api.route('/customers', methods=['GET'])
+def get_customers():
+    customers = Customer()
+    customers = customers.query.all()
+    customers = list(map(lambda item: item.serialize(), customers))
+    return jsonify(customers)
 
-    response_body = {
-        "message": "Hello! I'm a message that came from the backend, check the network tab on the google inspector and you will see the GET request"
-    }
-
-    return jsonify(response_body), 200
+@api.route('/customers/<int:customer_id>', methods=['GET'])
+def get_one_customer(customer_id = None):
+    if customer_id is not None:
+        customer = Customer()
+        customer = customer.query.get(customer_id)
+        if customer is not None:
+            return jsonify(customer.serialize()), 200
+        else:
+            return jsonify({"message":"customer not found"}), 404
+    else:
+        return jsonify({"message":"bad request"}), 400
