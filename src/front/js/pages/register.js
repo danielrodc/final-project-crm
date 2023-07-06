ac42f301b71c0444446683a1edc69cc769c36aa8
Paola Mangelli <mangelli.paola90@gmail.com>
2023-07-05 21:27:06 -0400

    register front upgrade 2

--- src/front/js/pages/register.js
+++ src/front/js/pages/register.js
@@ -40,11 +40,9 @@ export const Register = () => {
     return (
     <>
         <div className="container">
-            <div className="col-12">
+            <div className="row">
                 <h1>{edit == false ? "Add a contact" : "Edit a contact"}</h1>
-                
-
-                {/* <div className="row mb-3">
+                <div className="row mb-3">
                     <label htmlFor="nameInput" className="form-label">
                         Full Name
                     </label>
@@ -53,63 +51,92 @@ export const Register = () => {
                     required
                     className="form-control"
                     id="nameInput"
-                    placeholder="John Doe"
-                    defaultValue={name ? name : ""}
+                    placeholder="Name Last Name"
+                    defaultValue={fullName ? fullName : ""}
                     />
                 </div>
                 <div className="mb-3">
                     <label htmlFor="emailInput" className="form-label">
-                Email address
-            </label>
-            <input
-              type="email"
-              required
-              className="form-control"
-              id="emailInput"
-              placeholder="name@example.com"
-              defaultValue={email ? email : ""}
-            />
-          </div>
-          <div className="mb-3">
-            <label htmlFor="phoneInput" className="form-label">
-              Phone number
-            </label>
-            <input
-              type="number"
-              required
-              className="form-control"
-              id="phoneInput"
-              placeholder="000-000-0000"
-              defaultValue={phone ? phone : ""}
-            />
-          </div>
-          <div className="mb-3">
-            <label htmlFor="addressInput" className="form-label">
-              Address
-            </label>
-            <input
-              type="email"
-              required
-              className="form-control"
-              id="addressInput"
-              placeholder="Maracaibo, Zulia, VE"
-              defaultValue={address ? address : ""}
-            />
-          </div>
+                    Email address
+                    </label>
+                    <input
+                    type="email"
+                    required
+                    className="form-control"
+                    id="emailInput"
+                    placeholder="name@example.com"
+                    defaultValue={email ? email : ""}
+                    />
+                </div>
+                <div className="mb-3">
+                    <label htmlFor="phoneInput" className="form-label">
+                    Phone number
+                    </label>
+                    <input
+                    type="number"
+                    required
+                    className="form-control"
+                    id="phoneInput"
+                    placeholder="000-000-0000"
+                    defaultValue={phone ? phone : ""}
+                    />
+                </div>
+                <div className="mb-3">
+                    <label htmlFor="addressInput" className="form-label">
+                    Address
+                    </label>
+                    <input
+                    type="adress"
+                    required
+                    className="form-control"
+                    id="addressInput"
+                    placeholder="City, Country"
+                    defaultValue={address ? address : ""}
+                    />
+                </div>
+                <div className="mb-3">
+                    <label htmlFor="workRoll" className="form-label">
+                    Work Roll 
+                    </label>
+                    <input
+                    type="roll"
+                    required
+                    className="form-control"
+                    id="workRollInput"
+                    placeholder="Construction worker"
+                    defaultValue={workRoll ? workRoll : ""}
+                    />
+                </div>
+                <div>
+                    <label for="inputPassword5" className="form-label">
+                        Password
+                    </label>
+                    <input 
+                    type="password" 
+                    required
+                    id="inputPassword5" 
+                    className="form-control" 
+                    aria-labelledby="passwordHelpBlock"
+                    defaultValue={passWord ? passWord : ""}
+                    />
+                    <div id="passwordHelpBlock" className="form-text">
+                    Your password must be 8-20 characters long, contain letters and numbers, and must not contain spaces, special characters, or emoji.
+                    </div>
+                </div>
+            </div>
+            <div className="row d-flex justify-content-end">
+            <button className="col-2 ms-3 me-2">
+                <Link to="/login">Go back to Login</Link>
+            </button>
+            <button
+                type="button"
+                className="col-2 btn btn-primary me-3"
+                onClick={handleRegister}
+            >
+                {edit == false ? "Add contact" : "Edit contact"}
+            </button>
+            </div>
         </div>
-        <div className="row d-flex justify-content-end">
-          <button className="col-2 ms-3 me-2">
-            <Link to="/">Go back to contact list</Link>
-          </button>
-          <button
-            type="button"
-            className="col-2 btn btn-primary me-3"
-            onClick={handleDecide}
-          >
-            {edit == false ? "Add contact" : "Edit contact"}
-          </button>
-        </div>
-      </div> */}
     </>
     );
 };
